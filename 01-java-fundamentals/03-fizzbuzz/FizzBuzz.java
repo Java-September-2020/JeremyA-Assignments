a0fcd98265b245bc27e32973dcde53632725d62e
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-05 14:25:34 -0700

    Done

--- 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -1,3 +1,18 @@
 public class FizzBuzz {
+    public String fizzbuzz(int number) {
+        int two = 2;
+        if (number == 3){
+            return "Fizz";
+        }
+        else if (number == 5){
+            return "Buzz";
+        }
+        else if (number == 15){
+            return "FizzBuzz";
+        }
+        else {
+            return Integer.toString(number);
+        }
+    }
     
 }
--- 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 public class FizzBuzzTest {
     public static void main (String [] args) {
-        
+        FizzBuzz fizz = new FizzBuzz();
+        System.out.println(fizz.fizzbuzz(2));
     }
 }
