58596b0c70a9444be8dafa92fa60038b1a8f8168
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-05 15:07:19 -0700

    Chipping away

--- 01-java-fundamentals/04-string-manipulation/StringManipulator.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -1,5 +1,21 @@
 public class StringManipulator {
-    public String stringmanipulator() {
-
+    public String trimAndConcat(String str, String str2) {
+        return str;
+        return str2;
     }
+    public int getIndexOrNull(String str, char chr) {
+        return Interger.parseInt(str);
+        return chr;
+    }
+    public int getIndexOrNull(String str, String str2){
+        return Interger.parseInt(str);
+        return Interger.parseInt(str2);
+    }
+    public String concatSubstring(String str, int num, int num2, String str2) {
+        return str;
+        return Integer.toString(num);
+        return Integer.toString(num2);
+        return str2;
+    }
+
 }
