d6dc5f79bd2da42770334f0bc23b5e108ad39959
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-05 18:40:13 -0700

    2 arrays still need work, completed otherwise.

--- 01-java-fundamentals/05-basics/Basics.java
+++ 01-java-fundamentals/05-basics/Basics.java
@@ -1,86 +1,133 @@
+//IMPORTANT: for program to work, secions need to be tested, commented-out, and compilled indivudually. 
+
 import java.util.ArrayList;
 
 public class Basics {
     public static void main (String [] args){
-    // Print 1-255
-    for (int i = 1; i <= 255; i++){
-        System.out.println(i);
-    }
+        // Print 1-255
+        for (int i = 1; i <= 255; i++){
+            System.out.println(i);
+        }
 
-    // Print odd numbers between 1-255
-    for (int i = -1; i <= 255; i+=2){
-        System.out.println(i);
-    }
+        // // Print odd numbers between 1-255
+        // for (int i = -1; i <= 255; i+=2){
+        //     System.out.println(i);
+        // }
 
-    // Print Sum
-    int sum = 0;
-    for (int i = 0; i <= 255; i++){
-        sum = sum + i;
-        System.out.println("New number: " + i + " Sum: " + sum);
-    }
-
-
-    // Iterating through an array
-    int array [] = {1,3,5,7,9,13};
-    for (int i = 0; i < array.length; i++){
-        System.out.println(array[i]);
-    }
+        // // Print Sum
+        // int sum = 0;
+        // for (int i = 0; i <= 255; i++){
+        //     sum = sum + i;
+        //     System.out.println("New number: " + i + " Sum: " + sum);
+        // }
 
 
-    // Find Max
-    int array[] = {-3,-5,-7};
-    int max = 0;
-    for (int i = 0; i < array.length; i++){
-        max = max + array[i];
-    }
-    System.out.println(max);
+        // // Iterating through an array
+        // int array [] = {1,3,5,7,9,13};
+        // for (int i = 0; i < array.length; i++){
+        //     System.out.println(array[i]);
+        // }
 
 
-    // Get Average
-    int array[] = {2,10,3};
-    int sum = 0;
-    int avg = 0;
-    for (int i = 0; i < array.length; i++){
-        sum += array[i];
-    }
-    avg = sum / array.length;
-    System.out.println(avg);
+        // // Find Max
+        // int array[] = {-3,-5,-7};
+        // int max = 0;
+        // for (int i = 0; i < array.length; i++){
+        //     max = max + array[i];
+        // }
+        // System.out.println(max);
 
 
-    // Array with Odd Numbers
-    // Write a method that creates an array 'y' that contains all the odd numbers between 1 to 255. 
-    // When the method is done, 'y' should have the value of [1, 3, 5, 7, ... 255].
+        // // Get Average
+        // int array[] = {2,10,3};
+        // int sum = 0;
+        // int avg = 0;
+        // for (int i = 0; i < array.length; i++){
+        //     sum += array[i];
+        // }
+        // avg = sum / array.length;
+        // System.out.println(avg);
 
 
-    // Greater Than Y
-    // Write a method that takes an array and returns the number of values in that array whose 
-    // value is greater than a given value y. For example, if array = [1, 3, 5, 7] and y = 3, 
-    // after your method is run it will print 2 
-    // (since there are two values in the array that are greater than 3).
+        // // Array with Odd Numbers
+        // ArrayList<Integer> arrOdd = new ArrayList<Integer>();
+        // for (int i = 1; i <= 255; i+=2){
+        //     arrOdd.add(i);
+        // }
+        // int y []= new int[arrOdd.size()];
+        // for (int i = 0; i < y.length; i++){
+        //     y[i] = arrOdd.get(i);
+        // }
+        // System.out.println(y);
+        // // Will come back to this top one...
+
+        // // Greater Than Y
+        // int grtrArr[] = {1,3,5,7};
+        // int y = 3;
+        // int count = 0;
+        // for (int i = 0; i < grtrArr.length; i++){
+        //     if (grtrArr[i] > y){
+        //         count++;
+        //     }
+        // }
+        // System.out.println(count);
+
+        // // Square the values
+        // int arrX[] = {1,5,10,-2};
+        // for (int i = 0; i < arrX.length; i++) {
+        //     arrX[i] = arrX[i]*arrX[i];
+        //     System.out.println(arrX[i]);
+        // }
+
+        // // Eliminate Negative Numbers
+        // int arrNeg[] = {1,5,10,-2};
+        // for (int i = 0; i < arrNeg.length; i++) {
+        //     if (arrNeg[i] < 0){
+        //         arrNeg[i] = 0;
+        //     }
+        //     System.out.println(arrNeg[i]);
+        // }
+
+        // // Max, Min, and Average
+        // int arrMaxMinAvgX[] = {1,5,10,-2};
+        // int arrMax = 0;
+        // int arrMin = 0;
+        // int arrAvg = 0;
+        // int sum2 = 0;
+        // for (int i = 0; i < arrMaxMinAvgX.length; i++){
+        //     if (i == 0){
+        //         arrMax = arrMaxMinAvgX[i];
+        //         arrMin = arrMaxMinAvgX[i];
+        //     }
+        //     else {
+        //         if (arrMaxMinAvgX[i] > arrMax){
+        //             arrMax = arrMaxMinAvgX[i];
+        //         }
+        //         if (arrMaxMinAvgX[i] < arrMin){
+        //             arrMin = arrMaxMinAvgX[i];
+        //         }
+        //     }
+        //     sum2 = sum2 + arrMaxMinAvgX[i];
+        //     arrAvg = sum2/arrMaxMinAvgX.length;
+        // }
+        // System.out.println(arrMax + " " + arrMin + " " + arrAvg);
 
 
-    // Square the values
-    // Given any array x, say [1, 5, 10, -2], write a method that multiplies each value in the 
-    // array by itself. When the method is done, the array x should have values that have been 
-    // squared, say [1, 25, 100, 4].
-
-
-    // Eliminate Negative Numbers
-    // Given any array x, say [1, 5, 10, -2], write a method that replaces any negative number 
-    // with the value of 0. When the method is done, x should have no negative values, 
-    // say [1, 5, 10, 0].
-
-
-    // Max, Min, and Average
-    // Given any array x, say [1, 5, 10, -2], write a method that returns an array with the maximum 
-    // number in the array, the minimum value in the array, and the average of the values in the array. 
-    // The returned array should be three elements long and contain: [MAXNUM, MINNUM, AVG]
-
-
-    // Shifting the Values in the Array
-    // Given any array x, say [1, 5, 10, 7, -2], write a method that shifts each number by one to the 
-    // front. For example, when the method is done, an x of [1, 5, 10, 7, -2] should become [5, 10, 7, -2, 0]. 
-    // Notice that the last number is 0. The method does not need to wrap around 
-    // the values shifted out of bounds.
-    }    
+        // // Shifting the Values in the Array
+        // // Given any array x, say [1, 5, 10, 7, -2], write a method that shifts each number by one to the 
+        // // front. For example, when the method is done, an x of [1, 5, 10, 7, -2] should become [5, 10, 7, -2, 0]. 
+        // // Notice that the last number is 0. The method does not need to wrap around 
+        // // the values shifted out of bounds.
+        // int[] arrShift = {1, 5, 10, 7, -2};
+        // for (int i = 0; i < arrShift.length; i++){
+        //     if(i == arrShift.length - 1){
+        //         arrShift[i] = 0;
+        //     }
+        //     else{
+        //         arrShift[i] = arrShift[i+1];
+        //     }
+        //     System.out.println(arrShift[i]);
+        // }
+        // // Will come back to this top one...
+    } 
 }
