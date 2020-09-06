8055a5c12c84dfc522217bb5bac5c1ee6c679143
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-06 16:00:49 -0700

    Working Through It.

--- 02-java-oop/01-project-class/Project.java
+++ 02-java-oop/01-project-class/Project.java
@@ -1,3 +1,19 @@
 public class Project {
-    
+    private String name;
+    private String description;
+    public Project() {
+    }
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    public void elevatorPitch () {
+        System.out.println("My name is: " + this.getClass(Project(name)));
+        System.out.println("I am :" + this.getClass(Project(description)));
+    }
 }
