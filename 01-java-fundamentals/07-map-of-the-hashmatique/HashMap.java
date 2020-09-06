a1845a80a9c0e548e785f347d3c564e5dc12e424
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-06 10:40:36 -0700

    Pushing Through

--- 01-java-fundamentals/07-map-of-the-hashmatique/HashMap.java
+++ 01-java-fundamentals/07-map-of-the-hashmatique/HashMap.java
@@ -4,8 +4,8 @@ public class HashMap {
     public static void main(String[] args) {
         HashMap<String, String> trackList = new HashMap<String, String>();
         trackList.put("ABC by Jackson 5", "ABC, is easy as 123");
-        trackList.put("Miami 2 Ibiza by Swedish House Mafia & Tinie Tempah", "Boy I must be FIFA and that's standard
-        Procedure from Miami to Ibiza");
+        trackList.put("Miami 2 Ibiza by Swedish House Mafia & Tinie Tempah", 
+        "Boy I must be FIFA and that's standard procedure from Miami to Ibiza");
         trackList.put("Twisted by ATB", "Isn't that twisted, my love");
         trackList.put("Only You by Kaskade by Kaskade", "I will always know, only you");
 
@@ -13,9 +13,6 @@ public class HashMap {
         System.out.println("Title : " + title);
         
         Set<String> keys = trackList.keySet();
-        for(String key : keys) {
-            System.out.println(key);
-            System.out.println("Track :" trackList.get(key));    
-        }
+        System.out.println(keys);
     }
 }
