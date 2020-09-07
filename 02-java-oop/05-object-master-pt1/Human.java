eba9c8f14587b529feda08442ca90995d95ec46f
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-07 16:47:24 -0700

    Compiled

--- 02-java-oop/05-object-master-pt1/Human.java
+++ 02-java-oop/05-object-master-pt1/Human.java
@@ -1,3 +1,18 @@
 public class Human {
-    
+    int health;
+    int strength;
+    int stealth;
+    int intelligence;
+
+    public Human () {
+        health = 100;
+        strength = 3;
+        stealth = 3;
+        intelligence = 3;
+    }
+    public int attack() {
+        health = health - strength;
+        return health;
+    }
+
 }
--- 02-java-oop/05-object-master-pt1/Samurai.java
+++
@@ -1,3 +0,0 @@
-public class Samurai extends Human {
-    
-}
