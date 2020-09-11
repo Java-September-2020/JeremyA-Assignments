b4b865c67c0f22a7e1fa4a05d2eba9139c501fb5
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-10 18:39:25 -0700

    Added files

--- 02-java-oop/05-object-master-pt1/HumanTest.java
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -1,7 +1,6 @@
 public class HumanTest {
     public static void main (String[] args) {
         Human hmn = new Human();
-
         hmn.attack();
         System.out.println("Human's health: " + hmn.health);
     }
