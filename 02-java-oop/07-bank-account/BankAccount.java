01441b7c9f74e9b6289d370860e876bdb12900d4
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-11 20:14:51 -0700

    Chipping away.

--- 02-java-oop/07-bank-account/BankAccount.java
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -1,3 +1,14 @@
 public class BankAccount {
-    
+    private String accountNumber;
+    private double checkingBalance;
+    private double savingsBalance;
+
+    public static void Accounts (){
+    }
+
+    public static AccountBalance (){
+        return this.checkingBalance;
+    }
+
+    long number = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
 }
