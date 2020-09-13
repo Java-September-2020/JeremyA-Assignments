6bbd8f2dc7b118eab7096b0d4cd626e19da7b2de
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-13 09:34:52 -0700

    Pushing Through

--- 02-java-oop/07-bank-account/BankAccount.java
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -1,19 +1,74 @@
 public class BankAccount {
     private String accountNumber;
-    private double checkingBalance;
-    private double savingsBalance;
+	private double checkingBalance;
+	private double savingBalance;
+	public static int accountTotal = 0;
+	public static double balanceStored = 0;
+	
+	public BankAccount() {
+		accountNumber = randomAccountNum();
+		accountTotal++;
+	}
+	
+	private static String randomAccountNum() {
+        StringBuilder word = new StringBuilder(10); 
+        String randomAccountString = "0123456789";
+        for (int i = 0; i < 10; i++){
+            int index = (int)(randomAccountString.length() * Math.random());
+            word.append(randomAccountString.charAt(index));
+        }
+        String words = String.valueOf(word.toString());
+        return words;
+	}
+	
+	public double getCheckingBalance() {
+		return checkingBalance;
+	}
+	
+	public double getSavingBalance() {
+		return savingBalance;
+	}
+	
+	public void depositMoney(String accountName, double amount) {
+		if (accountName.equals("Checking")) {
+			System.out.println("$" + amount + " added to Checking Balance.");
+			checkingBalance += amount;
+		}
+		
+		if (accountName.equals("Savings")) {
+			System.out.println("$" + amount + " added to Saving Balance.");
+			savingBalance += amount;
+		}
+	}
+	
+	public void withdrawMoney(String accountName, double amount) {
+		if (accountName.equals("Checking")) {
+			if (checkingBalance - amount >= 0) {
+				System.out.println("$" + amount + " withdrawn to Checking Balance");
+				checkingBalance -= amount;
+			}
+			else {
+                System.out.println("Checking Account has: $" + checkingBalance + 
+                ". Can't withdraw: $" + amount);
+			}
 
-    public static void Accounts (){
-    }
+		}
+		
+		if (accountName.equals("Savings")) {
+			if (savingBalance - amount >= 0) {
+				System.out.println("$" + amount + " withdrawn to Savings Balance");
+				savingBalance -= amount;
+			}
+			else {
+                System.out.println("Savings Account has: $" + savingBalance + 
+                ". Can't withdraw: $" + amount);
+			}
 
-    public static AccountBalance (){
-        return this.checkingBalance;
-    }
-
-    private long randomAcctNum;
-    
-    //will be back to complete this...
-    = (long) Math.floor(Math.random() * 9_000_000_000L) + 1_000_000_000L;
-
-
+		}
+	}
+	
+	public void totalMoney() {
+        System.out.println("Checking & Savings Accounts have $" + (savingBalance + checkingBalance)
+        + " total.");
+	}
 }
