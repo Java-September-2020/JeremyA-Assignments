8d29869d29530b0fdf73952d2c522ae637fb50c5
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-13 17:42:21 -0700

    Done.

--- 03-java-spring/01-spring-fundamentals/01-strings/src/main/java/com/jeremyakatsa/strings/Application.java
+++ 03-java-spring/01-spring-fundamentals/01-strings/src/main/java/com/jeremyakatsa/strings/Application.java
@@ -2,12 +2,24 @@ package com.jeremyakatsa.strings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	@RequestMapping("/")
+    public String hello() {
+            return "Hello client, how are you?!";
+    }
+	
+	@RequestMapping("/random")
+    public String rando() {
+            return "Spring Boot is great, so easy to just respond with strings!";
+    }
 
 }
