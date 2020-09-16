b5b57e538281c2ed390edf756916781f25f08edc
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-15 19:14:47 -0700

    Done.

--- 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/src/main/java/com/jeremyakatsa/routing/controllers/DojoController.java
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/src/main/java/com/jeremyakatsa/routing/controllers/DojoController.java
@@ -1,5 +1,6 @@
 package com.jeremyakatsa.routing.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class DojoController {
 	@RequestMapping("/{dojo}")
-	public String hello(@PathVariable String dojo) {
-		return "Hello Coding Dojo!";
+	public String dojo(@PathVariable String dojo) {
+		return "The dojo is awesome!";
+	}
+	@RequestMapping("/{dojo}/{burbank}")
+	public String burbank(@PathVariable String burbank) {
+		return "Burbank Dojo is located in Southern California!";
+	}
+	@RequestMapping("/dojo/{san-jose}")
+	public String sanJose(@PathVariable("san-jose") String sanJose, Model model) {
+				model.addAttribute("san-jose", sanJose);
+		return "SJ dojo is the headquarters!";
 	}
 }
