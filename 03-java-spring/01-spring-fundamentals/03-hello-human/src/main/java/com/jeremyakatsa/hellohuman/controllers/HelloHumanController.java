19d9d95a19fa255963eb3c17e5065452f0ed3aba
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-17 18:50:51 -0700

    Working On It

--- 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/java/com/jeremyakatsa/hellohuman/controllers/HelloHumanController.java
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/java/com/jeremyakatsa/hellohuman/controllers/HelloHumanController.java
@@ -1,12 +1,13 @@
 package com.jeremyakatsa.hellohuman.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HelloHumanController {
 	@RequestMapping("")
 	public String helloHuman() {
-		return index.jsp;
+		return index.jsp; 
 	}
 }
