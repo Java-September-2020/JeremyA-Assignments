db5360244201b9ce94131cf34246b5d4ef7611ca
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-18 18:22:19 -0700

    Working on it

--- 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/java/com/jeremyakatsa/hellohuman/controllers/HelloHumanController.java
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/java/com/jeremyakatsa/hellohuman/controllers/HelloHumanController.java
@@ -1,21 +1,17 @@
 package com.jeremyakatsa.hellohuman.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("")
 public class HelloHumanController {
 	
 	@RequestMapping("/")
-	public String helloHuman() {
-		return index.jsp; 
-	}
-	
-	@RequestMapping("/{name}")
-	public String helloPerson(@RequestParam(value="q") String name) {
-        return index2.jsp;
-	}
+	public String helloHuman(Model model, @RequestParam(value="name", required=false, defaultValue="Human") String name) {
+		model.addAttribute("name", name);
+		return "index.jsp";
+		}
 	
 }
--- 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/webapp/WEB-INF/index.jsp
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/src/main/webapp/WEB-INF/index.jsp
@@ -10,6 +10,7 @@
 			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
 			crossorigin="anonymous">	
 		<meta charset="ISO-8859-1">
+		<link rel="stylesheet" href="main.css">
 	<title>Hello Human</title>
 </head>
 	<body>
