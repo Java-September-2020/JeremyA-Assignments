e1332a961a738be862e2d6210b861ec6ea8bedfc
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-22 19:00:51 -0700

    Working On.

--- 03-java-spring/02-spring-data-i/02-update-and-delete/src/main/java/com/jeremyakatsa/updatedelete/Application.java
+++ 03-java-spring/02-spring-data-i/02-update-and-delete/src/main/java/com/jeremyakatsa/updatedelete/Application.java
@@ -2,7 +2,12 @@ package com.jeremyakatsa.updatedelete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.jeremyakatsa.updatedelete.models"})
+@ComponentScan({"com.jeremyakatsa.updatedelete.repositories"})
+@ComponentScan({"com.jeremyakatsa.updatedelete.services"})
+@ComponentScan({"com.jeremyakatsa.updatedelete.controllers"})
 @SpringBootApplication
 public class Application {
 
---
+++ 03-java-spring/02-spring-data-i/02-update-and-delete/src/main/java/com/jeremyakatsa/updatedelete/controllers/BooksApi.java
@@ -0,0 +1,38 @@
+package com.jeremyakatsa.updatedelete.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeremyakatsa.updatedelete.models.Book;
+import com.jeremyakatsa.updatedelete.services.BookService;
+
+@RestController
+public class BooksApi {
+    private BookService bookService;
+    
+    public void Books(BookService bookService){
+        this.bookService = bookService;
+    }
+    
+    @RequestMapping("/api/books")
+    public List<Book> index() {
+        return bookService.allBooks();
+    }
+    
+    @RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
+    
+    @RequestMapping("/api/books/{id}")
+    public Book show(@PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        return book;
+    }
+}
