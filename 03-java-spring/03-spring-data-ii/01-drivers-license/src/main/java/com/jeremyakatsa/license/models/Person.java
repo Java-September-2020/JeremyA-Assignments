aa058ec82aab6dfa908fcf765b089e26292d7b04
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-09-29 18:24:49 -0700

    Punching Through

--- 03-java-spring/03-spring-data-ii/01-drivers-license/src/main/java/com/jeremyakatsa/license/models/Person.java
+++ 03-java-spring/03-spring-data-ii/01-drivers-license/src/main/java/com/jeremyakatsa/license/models/Person.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+@Entity
 @Table(name="persons")
 public class Person {
     
