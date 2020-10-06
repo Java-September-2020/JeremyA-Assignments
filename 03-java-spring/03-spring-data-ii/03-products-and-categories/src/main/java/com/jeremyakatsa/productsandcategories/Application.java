a38d4d3d9288a24aa3f15e441168ad184ab462d1
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-10-05 18:45:48 -0700

    Nowhere close to being finished, but I must move on.

--- 03-java-spring/03-spring-data-ii/03-products-and-categories/src/main/java/com/jeremyakatsa/productsandcategories/Application.java
+++ 03-java-spring/03-spring-data-ii/03-products-and-categories/src/main/java/com/jeremyakatsa/productsandcategories/Application.java
@@ -2,8 +2,12 @@ package com.jeremyakatsa.productsandcategories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan({"com.jeremyakatsa.productsandcategories.repositories"})
+@ComponentScan({"com.jeremyakatsa.productsandcategories.controllers"})
+@ComponentScan({"com.jeremyakatsa.productsandcategories.models"})
+@ComponentScan({"com.jeremyakatsa.productsandcategories.services"})
 @SpringBootApplication
 public class Application {
 
