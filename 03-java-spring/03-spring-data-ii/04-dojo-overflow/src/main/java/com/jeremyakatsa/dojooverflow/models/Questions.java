be542a23cbb963952fbe5ab9078b3cd521b38023
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-10-06 18:33:29 -0700

    Clocking out the Models

---
+++ 03-java-spring/03-spring-data-ii/04-dojo-overflow/src/main/java/com/jeremyakatsa/dojooverflow/models/Questions.java
@@ -0,0 +1,86 @@
+package com.jeremyakatsa.dojooverflow.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="questions")
+public class Questions {
+	
+	//Methods
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String question;
+	@Column(updatable=false)
+	private Date createdAt;
+	private Date updatedAt;
+	@OneToMany(mappedBy="question", fetch=FetchType.LAZY)
+	private List<Answer> answers;
+	
+	//Constructor
+	public Questions() {
+
+	}
+	
+	//Getters & Setters
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getQuestion() {
+		return question;
+	}
+
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+	
+
+}
