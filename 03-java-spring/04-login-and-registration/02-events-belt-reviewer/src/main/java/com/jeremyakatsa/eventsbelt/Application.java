a0fcc1c12f08c90a409c95ecda1fe4a1f87e4339
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-10-21 15:45:52 -0700

    AWS work-around

--- 03-java-spring/04-login-and-registration/02-events-belt-reviewer/src/main/java/com/jeremyakatsa/eventsbelt/Application.java
+++ 03-java-spring/04-login-and-registration/02-events-belt-reviewer/src/main/java/com/jeremyakatsa/eventsbelt/Application.java
@@ -17,7 +17,6 @@ public class Application {
     public TomcatServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
         Connector ajpConnector = new Connector("AJP/1.3");
-        ajpConnector.getPort(9090);
         ajpConnector.setPort(9090);
         ajpConnector.setSecure(false);
         ajpConnector.setAllowTrace(false);
